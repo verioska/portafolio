06b56583efa760124fc321a5d07395fc68f52838
verioska <rverioska01@gmail.com>
2019-03-27 21:01:36 -0300

    cambiando el menu

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.fixed-action-btn');
+//     var instances = M.FloatingActionButton.init(elems, options);
+//   });
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.fixed-action-btn');
+//     var instances = M.FloatingActionButton.init(elems, {
+//       toolbarEnabled: true
+//     });
+//   })
 
 let i=0;
 document.getElementById("toggle").addEventListener("click", function(){
@@ -8,21 +17,58 @@ if(i==0){
 
     document.getElementById("menu").style.
      transform='scale(2)';
-     document.getElementById("bar").style.
-     transform='rotate(70deg)';
+    //  document.getElementById("bar").style.
+    //  transform='rotate(70deg)';
      i=1;
 }else{
 
     document.getElementById("menu").style.
     transform='scale(0)';
-    document.getElementById("bar").style.
-    transform='rotate(35deg)';
+    // document.getElementById("bar").style.
+    // transform='rotate(35deg)';
     i=0;
 }
   })
 
   document.getElementById("home").addEventListener("click", function(){
    
-   alert("holaaa soy verioska")
-      })
+    document.getElementById("section2").style.display="none";
+    document.getElementById("section1").style.display="block";
+    document.getElementById("section3").style.display="none";
+    document.getElementById("section4").style.display="none";
+    document.getElementById("section5").style.display="none";
+})
+  document.getElementById("user").addEventListener("click", function(){
+   
+    document.getElementById("section2").style.display="block";
+    document.getElementById("section1").style.display="none";
+    document.getElementById("section3").style.display="none";
+    document.getElementById("section4").style.display="none";
+    document.getElementById("section5").style.display="none";
+})
+ document.getElementById("habilidades").addEventListener("click", function(){
+
+    document.getElementById("section3").style.display="block";
+    document.getElementById("section2").style.display="none";
+    document.getElementById("section1").style.display="none";
+    document.getElementById("section4").style.display="none";
+    document.getElementById("section5").style.display="none";
+  
+})
+document.getElementById("proyectos").addEventListener("click", function(){
+
+    document.getElementById("section4").style.display="block";
+    document.getElementById("section2").style.display="none";
+    document.getElementById("section1").style.display="none";
+    document.getElementById("section3").style.display="none";
+    document.getElementById("section5").style.display="none";
+})
+document.getElementById("mail").addEventListener("click", function(){
+
+    document.getElementById("section5").style.display="block";
+    document.getElementById("section4").style.display="none";
+    document.getElementById("section2").style.display="none";
+    document.getElementById("section1").style.display="none";
+    document.getElementById("section3").style.display="none";
+})
 
